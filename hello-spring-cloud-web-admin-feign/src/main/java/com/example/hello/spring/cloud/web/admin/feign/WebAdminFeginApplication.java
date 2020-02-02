ac6955a7ccff161b37b38c4c5152468f1a2d024a
wjn0918 <wjn_0918@163.com>
2020-02-02 15:27:42 +0800

    增加熔断器监控

--- hello-spring-cloud-web-admin-feign/src/main/java/com/example/hello/spring/cloud/web/admin/feign/WebAdminFeginApplication.java
+++ hello-spring-cloud-web-admin-feign/src/main/java/com/example/hello/spring/cloud/web/admin/feign/WebAdminFeginApplication.java
@@ -3,10 +3,12 @@ package com.example.hello.spring.cloud.web.admin.feign;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @EnableDiscoveryClient
+@EnableHystrixDashboard
 @SpringBootApplication
 public class WebAdminFeginApplication {
 
